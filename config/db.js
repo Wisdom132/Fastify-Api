a1f09df5678adf815a72425720b62bbca287d506
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-04-20 13:44:28 +0100

    added error handling

--- config/db.js
+++ config/db.js
@@ -3,11 +3,17 @@ const mongoose = require('mongoose')
 
 // Connect to DB
 async function dbConnector(fastify, options) {
-    const url = "mongodb://localhost:27017/fastify-blog"
-    const db = await mongoose
-        .connect(url, {
-            useNewUrlParser: true
-        })
-    fastify.decorate('mongo', db)
+    try {
+        const url = "mongodb://localhost:27017/fastify-blog"
+        const db = await mongoose
+            .connect(url, {
+                useNewUrlParser: true
+            })
+        console.log("Database is connected")
+        fastify.decorate('mongo', db)
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 module.exports = fastifyPlugin(dbConnector)
